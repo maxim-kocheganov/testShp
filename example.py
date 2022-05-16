2ce87010e130acde3632f79efd7cf23805f35362
max <maxim.k.elasnce@gmail.com>
2022-05-16 14:34:00 +0300

    ffff

--- example.py
+++ example.py
@@ -1,2 +1,101 @@
-from fastapi import FastAPI
-app = FastAPI
+from fastapi import FastAPI, Body, Depends
+app = FastAPI()
+from app.model import PostSchema
+from app.model import PostSchema, UserSchema, UserLoginSchema
+from app.jwt import signJWT
+from app.jwt_bearer import JWTBearer
+
+"""
+Дневник программиста.
+
+1-ый день. Поставил Windows 3.11 - я тащусь.
+
+2-ой день. Поставил Windows 95 - кайф.
+
+3-ий день. Поставил Windows NT - такого кайфа я не испытывал никогда в жизни.
+
+4-ый день. Был с женщиной - жалкое подобие 3.11.
+"""
+
+
+posts = [
+    {
+        "id": 1,
+        "title": "Penguins ",
+        "text": "Penguins are a group of aquatic flightless birds."
+    },
+    {
+        "id": 2,
+        "title": "Tigers ",
+        "text": "Tigers are the largest living cat species and a memeber of the genus panthera."
+    },
+    {
+        "id": 3,
+        "title": "Koalas ",
+        "text": "Koala is arboreal herbivorous maruspial native to Australia."
+    },
+]
+
+users = []
+
+app = FastAPI()
+
+
+
+def check_user(data: UserLoginSchema):
+    for user in users:
+        if user.email == data.email and user.password == data.password:
+            return True
+    return False
+
+
+# route handlers
+
+# testing
+@app.get("/", tags=["test"])
+def greet():
+    return {"hello": "world!."}
+
+
+# Get Posts
+@app.get("/posts", tags=["posts"])
+def get_posts():
+    return { "data": posts }
+
+
+@app.get("/posts/{id}", tags=["posts"])
+def get_single_post(id: int):
+    if id > len(posts):
+        return {
+            "error": "No such post with the supplied ID."
+        }
+
+    for post in posts:
+        if post["id"] == id:
+            return {
+                "data": post
+            }
+
+
+@app.post("/posts", dependencies=[Depends(JWTBearer())], tags=["posts"])
+def add_post(post: PostSchema):
+    post.id = len(posts) + 1
+    posts.append(post.dict())
+    return {
+        "data": "post added."
+    }
+
+
+@app.post("/user/signup", tags=["user"])
+def create_user(user: UserSchema = Body(...)):
+    users.append(user) # replace with db call, making sure to hash the password first
+    return signJWT(user.email)
+
+
+@app.post("/user/login", tags=["user"])
+def user_login(user: UserLoginSchema = Body(...)):
+    if check_user(user):
+        return signJWT(user.email)
+    return {
+        "error": "Wrong login details!"
+    }
